a966188bd542a84c75a0c52c65ad2b320daf8889
Tarek Badr <tarekbadrshalaan@gmail.com>
2019-01-25 12:13:49 +0200

    add random

--- tutorialspoint/10-c_arrays/random.c
+++ tutorialspoint/10-c_arrays/random.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h> 
+#include <time.h>
 
 // getRandom : generate array of random numbers.
-
-/*
+ 
 int * getRandom( ) {
 
    static int  r[10];
@@ -16,21 +17,6 @@ int * getRandom( ) {
    }
 
    return r;
-}
-*/
-
-int *getRandom()
-{
-
-    static int r[10];
-
-    for (int i = 0; i < 10; ++i)
-    {
-        r[i] = i + 100;
-        printf("r[%d] = %d\n", i, r[i]);
-    }
-
-    return r;
 }
 
 int main()
