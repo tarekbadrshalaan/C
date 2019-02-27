4b4dd2c95ad420c96071af8cc0f8ad5693f5792e
Tarek Badr <tarekbadrshalaan@gmail.com>
2019-02-27 09:27:17 +0200

    add recursion

---
+++ tutorialspoint/21-c_recursion/recursion.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+unsigned long long int factorial(unsigned int i)
+{
+    if (i <= 1)
+    {
+        return 1;
+    }
+    return i * factorial(i - 1);
+}
+
+int fibonacci(int i)
+{
+    if (i == 0)
+    {
+        return 0;
+    }
+    if (i == 1)
+    {
+        return 1;
+    }
+    return fibonacci(i - 1) + fibonacci(i - 2);
+}
+
+int main()
+{
+    int i = 12;
+    printf("Factorial of %d is %lld\n", i, factorial(i));
+
+    
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d\t\n", fibonacci(i));
+    }
+    return 0;
+}
